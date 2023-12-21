4cb74dba84f00cb174d098ffdf862d3863583510
Robert Ferguson <robertferguson78@gmail.com>
2023-12-21 01:07:54 +0000

    pass in logged in user id and station id as stings for comparison

--- controllers/dashboard-controller.js
+++ controllers/dashboard-controller.js
@@ -6,7 +6,19 @@ export const dashboardController = {
   // Initial index page to display with view data to pass in
   async index(request, response) {
     const loggedInUser = request.user._id;
+    const loggedInUserMap = request.user._id.toString(); // convert loggedInUser to string
     const stations = await stationStore.getStationsByUserId(loggedInUser);
+    const allStations = await stationStore.getAllStations(); // get all stations
+
+    // Loop through all stations to attach last reading
+    for (const station of allStations) {
+      const readingObject = await lastReadings(station._id);
+      // Add the Analytics reading to this station
+      Object.assign(station, readingObject.reading)
+      // Add the icon name
+      station.iconName = "a" + readingObject.reading.latestCode;
+    }
+
     let addStation = false;
     if (stations.length == 1) {
       addStation = true;
@@ -17,14 +29,24 @@ export const dashboardController = {
     // Data to pass into View
     const viewData = {
       title: "Station Dashboard",
+      allStations: allStations.map(station => ({ ...station, userid: station.userid.toString() })), // convert userid to string
       stations: stations,
       addStation: addStation,
+      loggedInUser: loggedInUser,
+      loggedInUserMap: loggedInUserMap, // add logged in user for dashboard map
     };
+
+    // debug why lohgged in user id and station id dont match
+    // console.log('loggedInUser:', loggedInUser.toString(), typeof loggedInUser.toString());
+    // allStations.forEach(station => {
+    //   console.log('station userid:', station.userid.toString(), typeof station.userid.toString());
+    // });
 
     // Loop through last reading in Utils/Analytics to diplay last readings based on stations in view data
     for (const station of viewData.stations) {
       const readingObject = await lastReadings(station._id);
       // Add the Analytics reading to this viewData
+      //console.log(allStations);
       Object.assign(station, readingObject.reading)
     }
 
