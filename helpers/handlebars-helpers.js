7fbf6841501118ee3dc8665e5ee470cea0074ea5
Robert Ferguson <robertferguson78@gmail.com>
2023-12-21 01:09:55 +0000

    create another helper function to check for equality and also a debug it not equal

--- helpers/handlebars-helpers.js
+++ helpers/handlebars-helpers.js
@@ -1,4 +1,7 @@
 export const handlebarsHelpers = {
-   // greater than or equal to
-   greaterOrEqual: (a, b) => a >= b
- };
+  // greater than or equal to
+  greaterOrEqual: (a, b) => a >= b,
+  // equal to
+  eq: (a, b) => a === b,
+  nq: (a, b) => a !== b,
+};
